79425f7c854095a4432c6d7e873927efbe1277c8
gfp340 <133774242+gfp340@users.noreply.github.com>
2026-04-24 23:07:30 +0800

    Create 18.user.php

---
+++ 18.user.php
@@ -0,0 +1,28 @@
+<html>
+    <head><title>使用者管理</title></head>
+    <body>
+    <?php
+#先隱藏錯誤，而後檢查是否有登入ID
+        error_reporting(0);
+        session_start();
+        if (!$_SESSION["id"]) {
+            echo "請登入帳號";
+            echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+        }
+  #底下是增加超連結按鈕跟帳秘輸入欄
+        else{   
+            echo "<h1>使用者管理</h1>
+                [<a href=14.user_add_form.php>新增使用者</a>] [<a href=11.bulletin.php>回佈告欄列表</a>]<br>
+                <table border=1>
+                    <tr><td></td><td>帳號</td><td>密碼</td></tr>";
+  #輸入好後連接到120.105.96.90設定的資料庫，會驗證張浩與密碼是否一致      
+            $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
+            $result=mysqli_query($conn, "select * from user");
+            while ($row=mysqli_fetch_array($result)){
+                echo "<tr><td><a href=19.user_edit_form.php?id={$row['id']}>修改</a>||<a href=17.user_delete.php?id={$row['id']}>刪除</a></td><td>{$row['id']}</td><td>{$row['pwd']}</td></tr>";
+            }
+            echo "</table>";
+        }
+    ?> 
+    </body>
+</html>
